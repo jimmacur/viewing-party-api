0d28ca993cee26b5008234dedf8c74f80490efad
Jim Macur <jim.macur@gmail.com>
2024-10-10 15:39:15 -0600

    refactor: lowercase b

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,8 +1,8 @@
-require_relative '../../../services/movie_db_gateway'
+# require_relative '../../../services/movie_db_gateway'
 
 class Api::V1::MoviesController < ApplicationController
   def index
-    movies = MovieDBGateway.new.top_rated_movies
+    movies = MovieDbGateway.new.top_rated_movies
     render json: MovieSerializer.new(movies)
   end
 end
--- app/services/movie_db_gateway.rb
+++ app/services/movie_db_gateway.rb
@@ -1,4 +1,4 @@
-class MovieDBGateway
+class MovieDbGateway
   def top_rated_movies
     response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated", { api_key: ENV['MOVIE_DB_API_KEY'] })
     parsed_response = JSON.parse(response.body, symbolize_names: true)
