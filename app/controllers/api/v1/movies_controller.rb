7b9848554bb059b15cfaaf32bf642f82029df847
Jim Macur <jim.macur@gmail.com>
2024-10-10 15:33:25 -0600

    refactor: rename and move folders

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,15 +1,8 @@
+require_relative '../../../services/movie_db_gateway'
+
 class Api::V1::MoviesController < ApplicationController
   def index
-    movies = Movie.top_rated_movies
+    movies = MovieDBGateway.new.top_rated_movies
     render json: MovieSerializer.new(movies)
   end
-
-  # def search
-  #   if params[:query].present?
-  #     movies = MovieSearch.new(params[:query]).call
-  #     render json: MovieSerializer.new(movies)
-  #   else
-  #     render json: { error: "Search term missing" }, status: :bad_request
-  #   end
-  # end
 end
--- app/services/movie_db_gateway.rb
+++ app/services/movie_db_gateway.rb
@@ -1,11 +1,11 @@
-class Movie
-  def self.top_rated_movies
+class MovieDBGateway
+  def top_rated_movies
     response = Faraday.get("https://api.themoviedb.org/3/movie/top_rated", { api_key: ENV['MOVIE_DB_API_KEY'] })
     parsed_response = JSON.parse(response.body, symbolize_names: true)
 
     parsed_response[:results].map do |movie_data|
-      OpenStruct.new(
-        id: movie_data[:id].to_s,
+      Movie.new(
+        id: movie_data[:id],
         title: movie_data[:title],
         vote_average: movie_data[:vote_average]
       )
