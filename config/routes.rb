fcf7f03829968d428256d9627281a96763d523d0
Jim Macur <jim.macur@gmail.com>
2024-10-10 20:24:52 -0600

    feat: adds movie details serializer

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :index]
       resources :sessions, only: :create
-      resources :movies, only: [:index]
-      # get 'movies/search', to: 'movies#search'
+      resources :movies, only: [:index, :show]
     end
   end
 end
