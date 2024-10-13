c839f96ceee238c3165922882be5abc18407d4f3
Jim Macur <jim.macur@gmail.com>
2024-10-13 12:48:54 -0600

    feat: add to user serializer show route and controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :validate_api_key, only: [:show]
+
   def create
     user = User.new(user_params)
     if user.save
@@ -12,9 +14,25 @@ class Api::V1::UsersController < ApplicationController
     render json: UserSerializer.format_user_list(User.all)
   end
 
+  def show
+    user = User.find(params[:id])
+    if user
+      render json: UserSerializer.new(user)
+    else
+      render json: { message: 'User not found', status: 404 }, status not_found
+    end
+  end
+
   private
 
   def user_params
     params.permit(:name, :username, :password, :password_confirmation)
+  end
+
+  def validate_api_key
+    user = User.find_by(api_key: params[:api_key])
+    if user.nil? || user.api_key != params[:api_key]
+      render json: { message: 'Unauthorized', status: 401 }, status: :unauthorized
+    end
   end
 end
