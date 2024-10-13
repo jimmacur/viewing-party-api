8190abe3bba338190c1b822b74bf453eba9c3800
Jim Macur <jim.macur@gmail.com>
2024-10-12 21:25:02 -0600

    test: adds viewing party and invitation spec

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :viewing_party }
+  it { should belong_to :user }
 end
