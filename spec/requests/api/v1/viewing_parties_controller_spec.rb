c75ac364a1ba837b601752a71eebf883c7e980a5
Jim Macur <jim.macur@gmail.com>
2024-10-12 21:17:02 -0600

    test: updates viewing party controller spec for bad api

--- spec/requests/api/v1/viewing_parties_controller_spec.rb
+++ spec/requests/api/v1/viewing_parties_controller_spec.rb
@@ -26,4 +26,24 @@ RSpec.describe "Viewing Parties Controller" do
     expect(ViewingParty.count).to eq(1)
     expect(Invitation.count).to eq(2)
   end
+
+  it 'returns an error with an invalid API key' do
+    party_params = {
+        viewing_party: {
+          name: "Juliet's Bday Movie Bash!",
+          start_time: "2025-02-01 10:00:00",
+          end_time: "2025-02-01 14:30:00",
+          movie_id: 278,
+          movie_title: "The Shawshank Redemption"
+        },
+        api_key: 'invalid_key',
+        invitees: [@invitee1.id, @invitee2.id]
+      }
+
+    post '/api/v1/viewing_parties', params: party_params
+
+    expect(response).to have_http_status(:unauthorized)
+    expect(ViewingParty.count).to eq(0)
+    expect(Invitation.count).to eq(0)
+  end
 end
